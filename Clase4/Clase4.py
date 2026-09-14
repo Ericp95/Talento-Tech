bd587da76ac878963f7a5ea169bc6643ace341ac
Ericp95 <ericp6960@gmail.com>
2026-09-14 11:20:32 -0300

    haciendo clase4

--- Clase4/Clase4.py
+++ Clase4/Clase4.py
@@ -1,6 +1,12 @@
 nombre=input("nombre: ")
+apellido=input("apellido: ")
 
 if nombre == "" :
     print("ERROR")
 else:
-    print(nombre.capitalize)
+    print(nombre.capitalize())
+
+if apellido == "" :
+    print("ERROR")
+else:
+    print(apellido.capitalize())
