bfec4179f07c6fe3e509f3cee5a9107a31f9efec
Ericp95 <ericp6960@gmail.com>
2026-09-15 16:33:37 -0300

    creando acumulador ingresos

--- Clase5/Clase5.py
+++ Clase5/Clase5.py
@@ -1,11 +1,15 @@
-meses=1
+meses=0
 acum=0
 ingresos=0
 prom=0 
 
-while meses < 7 : 
+while meses < 6: 
     ingresos= int(input("agregar ingresos mensuales")) 
 
     if ingresos < 0 :
         print(" Error el ingreso es negativo")
+
+    if ingresos > 0 :
+        acum= acum + ingresos
+        meses= meses+1
         
