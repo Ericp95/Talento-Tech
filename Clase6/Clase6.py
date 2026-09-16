f437bdfabf4fa5c4dddcaf625a078ee42b7988fb
Ericp95 <ericp6960@gmail.com>
2026-09-16 15:32:40 -0300

    modificando funcion

--- Clase6/Clase6.py
+++ Clase6/Clase6.py
@@ -1,6 +1,7 @@
 
-lista= input("ingrese nombres que desea listar")
-array=lista.split()
+#lista= input("ingrese nombres que desea listar")
+#array=lista.split()
+nombres=["pedro", "juan","pablo","eric","", "juanita","lola"]
 
-for i in range(len(array)): 
-    print(f" Cliente{i+1}: {array[i]}")
+for i in range(len(nombres)): 
+    print(f" Cliente{i+1}: {nombres[i]}")
