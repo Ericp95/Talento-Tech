5c9660c2c6b0c0958210e522b392461862af3b94
Ericp95 <ericp6960@gmail.com>
2026-09-18 14:16:38 -0300

    haciendo manera mejor el for

--- Clase6/Clase6.py
+++ Clase6/Clase6.py
@@ -1,10 +1,16 @@
 
 #lista= input("ingrese nombres que desea listar")
 #array=lista.split()
-nombres=["pedro", "juan","pablo","eric","", "juanita","lola"]
+"""nombres=["pedro", "juan","pablo","eric","", "juanita","lola"]
 
 for i in range(len(nombres)): 
     if nombres[i] == "":
         print(f" Cliente{i+1}: Error nombre vacio")
     else:
         print(f" Cliente{i+1}: {nombres[i].capitalize()}")
+"""
+#haciendo una forma mejor
+
+nombre=[]
+
+
